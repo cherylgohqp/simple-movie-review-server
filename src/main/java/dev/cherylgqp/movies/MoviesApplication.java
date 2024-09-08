efb7905f1df3d796178e6c79c0022c87d443eb80
Cheryl <cherylgohqp@gmail.com>
2024-09-08 18:40:20 +0800

    quick test to see if the end point is working

---
+++ src/main/java/dev/cherylgqp/movies/MoviesApplication.java
@@ -0,0 +1,23 @@
+package dev.cherylgqp.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController // another annotation to let the framework knows that this class is actually a
+				// Rest API controller and not just another class
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+	// commented code is just a quick demo to test if our endpoint is working
+	// @GetMapping("/") // it lets the frameework knows that this method here is a
+	// get endpoint
+	// public String apiRoot() {
+	// return "Hello World!";
+	// }
+}
---
+++ src/test/java/dev/cherylgqp/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cherylgqp.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
