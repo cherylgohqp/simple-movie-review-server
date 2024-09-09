d60c297c3cdf4d4a27dc06945d2226d8587d3408
Cheryl <cherylgohqp@users.noreply.github.com>
2024-09-09 19:05:29 +0800

    added cross orgin annotation to controller classes Because the Tomcat’s port is 8080 and the React’s port is 3000, there is a cross-domain problem, and requests will be blocked by the CORS policy.

--- src/main/java/dev/cherylgqp/movies/MovieController.java
+++ src/main/java/dev/cherylgqp/movies/MovieController.java
@@ -7,14 +7,15 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/movies") // root of api
-
 // API Layer -> concerns about getting the req from the user and returning the
 // response
 public class MovieController {
